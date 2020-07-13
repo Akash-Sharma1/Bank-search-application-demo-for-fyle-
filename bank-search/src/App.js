f7a4b6950110c2adc76a10093f7e321a198c7193
Akash Sharma <aakashsharmajc100@gmail.com>
2020-07-14 00:37:25 +0530

    Inital commit

---
+++ bank-search/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import BankPage from './Components/BankPage';
+import SearchPage from './Components/SearchPage';
+require('dotenv').config()
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossOrigin="anonymous"/>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
+
+        <BankPage/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ bank-search/src/Components/BankPage.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+
+
+export default class BankPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isloading: -1,
+            Banklist: {},
+            SelectedCity: "MUMBAI",
+            Cities: ["MUMBAI", "DELHI", "KOLKATA", "BANGALORE", "CUTTACK"],
+            Pages: 10,
+            Rows: 5,
+            Onscreenlist: [],
+            Pagenumber: 0,
+            TotalPages: 0,
+        }
+        this.refreshlist = this.refreshlist.bind(this);
+        this.domatch = this.domatch.bind(this);
+    }
+
+    domatch(a, b) {
+
+    }
+
+    refreshlist() {
+        var banklist = this.state.Banklist[this.state.SelectedCity];
+
+        var search = document.getElementById("searchinput").value;
+
+        if (search != "") {
+            var templist = [];
+            for (var i = 0; i < banklist.length; i++) {
+
+            }
+        }
+
+        var totalP = parseInt(banklist.length / this.state.Rows);
+        var currpage = this.state.Pagenumber;
+        currpage = Math.max(0, currpage);
+        currpage = Math.min(totalP, currpage);
+        this.setState({
+            TotalPages: totalP,
+            Pagenumber: currpage
+        })
+        if (banklist.length % this.state.Rows > 0) totalP++;
+        var newlist = []
+        for (var i = currpage * this.state.Rows; i < currpage * this.state.Rows + this.state.Rows; i++) {
+            if (i >= banklist.length) break;
+            newlist.push(banklist[i]);
+        }
+        this.setState({ Onscreenlist: newlist });
+    }
+
+    componentDidMount() {
+        var done = -this.state.Cities.length;
+        var banklist = {};
+        for (var i = 0; i < this.state.Cities.length; i++) {
+
+            banklist[this.state.Cities[i]] = [];
+
+            axios.get(process.env.REACT_APP_BankListAPI + this.state.Cities[i])
+                .then((response) => {
+                    response = response.data;
+
+                    done++; this.setState({ isloading: done });
+
+                    banklist[response[0].city] = response;
+                    this.setState({ Banklist: banklist }, () => { this.refreshlist(); });
+                })
+                .catch(function (error) {
+                    alert(error);
+                })
+        }
+        this.setState({ Banklist: banklist });
+    }
+
+    render() {
+        let Datatobeshown;
+        if (this.state.isloading !== 0) { Datatobeshown = <div><h4 style={{ minHeight: "68vh", marginBottom: "5px" }}>Fetching Data...</h4></div> }
+        else if (this.state.Onscreenlist.length == 0) {
+            Datatobeshown = <h4>Nothing to Show... :(</h4>
+        } else {
+            Datatobeshown = <div style={{ minHeight: "68vh", marginBottom: "5px" }}>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Actions</th>
+                            <th>Bank Name</th>
+                            <th>IFSC code</th>
+                            <th>Bank ID</th>
+                            <th>Branch</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.Onscreenlist.map((B) => {
+                            return <tr key={B.ifsc}>
+                                <td>Fav?</td>
+                                <td style={{ width: "30%" }}>{B.bank_name}</td>
+                                <td style={{ width: "10%" }}>{B.ifsc}</td>
+                                <td style={{ width: "10%" }}>{B.bank_id}</td>
+                                <td style={{ width: "30%" }}>{B.branch}</td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        }
+        return (
+            <div style={{ height: "100%" }}>
+                <select className="form-control selectbox" onChange={(e) => {
+                    this.setState({ SelectedCity: e.target.value, Pagenumber: 0 }, () => { this.refreshlist(); })
+                }} >
+                    {this.state.Cities.map((c) => { return <option key={c}>{c}</option> })}
+                </select>
+
+                <div className="mainbody">
+                    <div style={{ minHeight: 36, marginBottom: "2%" }}>
+                        <span style={{ position: "absolute", left: "3%" }}>List of banks in {this.state.SelectedCity}</span>
+                        <span style={{ position: "absolute", right: "3%" }}>
+                            <div className="btn-group">
+                                <input id="searchinput" type="search" className="form-control" placeholder="Search" onChange={() => this.refreshlist()} />
+                                <span id="searchclear" className="glyphicon glyphicon-remove-circle" onClick={
+                                    () => { document.getElementById("searchinput").value = ""; this.refreshlist(); }
+                                }></span>
+                            </div>
+                        </span>
+                    </div>
+
+                    {Datatobeshown}
+
+                    <div style={{ display: "block", }}>
+                        <div style={{ display: "inline-block", width: "32%" }}>
+                            <div style={{ display: "inline-block", marginRight: "2%" }}>Rows per page: </div>
+                            <select className="form-control" style={{ display: "inline-block", width: "40%" }}
+                                onChange={(e) => {
+                                    this.setState({ Rows: parseInt(e.target.value) }, () => { this.refreshlist(); })
+                                }} >
+                                <option>5</option>
+                                <option>10</option>
+                                <option>15</option>
+                            </select>
+                        </div>
+                        <div style={{ display: "inline-block", width: "50%" }}>
+                            <img src={require('../chevron-left.svg')} alt="1" className="App-logo" onClick={() => {
+                                console.log("Clicked");
+                                this.setState({ Pagenumber: 0 }, () => { this.refreshlist(); })
+                            }} />
+                            <img src={require('../chevron-left (1).svg')} alt="2" className="App-logo" onClick={() => {
+                                this.setState({ Pagenumber: Math.max((parseInt(this.state.Pagenumber) - 1), 0) }, () => { this.refreshlist(); });
+                            }} />
+
+                            <div style={{ display: "inline-block", width: "30%", textAlign: "center" }}>Page number : {this.state.Pagenumber} of {this.state.TotalPages}</div>
+
+                            <img src={require('../chevron-right.svg')} alt="3" className="App-logo" onClick={() => {
+                                this.setState({ Pagenumber: Math.min((parseInt(this.state.Pagenumber) + 1), this.state.TotalPages) }, () => { this.refreshlist(); });
+                            }} />
+                            <img src={require('../chevron-right (1).svg')} alt="4" className="App-logo" onClick={() => {
+                                this.setState({ Pagenumber: this.state.TotalPages }, () => { this.refreshlist(); })
+                            }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
